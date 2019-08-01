b954b340e888dcf46e878bff08a66832a0012ced
Beverly Pham <beverly.pham@gmail.com>
2019-08-01 11:19:54 -0700

    add script to receive customer input and post to server

--- frontend/src/components/customer/Create.vue
+++ frontend/src/components/customer/Create.vue
@@ -33,3 +33,39 @@
         </div>
     </div>
 </template>
+
+<script>
+import axios from "axios";
+import { server } from "../../helper";
+import router from "../../router";
+export default {
+  data() {
+    return {
+      first_name: "",
+      last_name: "",
+      email: "",
+      phone: "",
+      address: "",
+      description: ""
+    };
+  },
+  methods: {
+    createCustomer() {
+      let customerData = {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        phone: this.phone,
+        address: this.address,
+        description: this.description
+      };
+      this.__submitToServer(customerData);
+    },
+    __submitToServer(data) {
+      axios.post(`${server.baseURL}/customer/create`, data).then(data => {
+        router.push({ name: "home" });
+      });
+    }
+  }
+};
+</script>
