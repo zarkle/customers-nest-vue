d011cd3de239dfa0c28bf08e923f745e3c80f2ec
Beverly Pham <beverly.pham@gmail.com>
2019-08-01 02:57:46 -0700

    set up customer module

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerSchema } from './schemas/customer.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Customer', schema: CustomerSchema }])
+  ],
   providers: [CustomerService],
   controllers: [CustomerController]
 })
